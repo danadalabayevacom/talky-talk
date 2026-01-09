7df5285688bee55352b73396663c35dd2b43064c
Dana <dana@example.com>
2026-01-09 21:37:06 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="border border-red-400 bg-amber-600">
+       Hello world
+      </div>
+      
+    </>
+  )
+}
+
+export default App
