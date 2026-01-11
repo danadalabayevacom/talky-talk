5fe5624c8b4254a92741ef43c4c40d01288a04f5
Dana <dana@example.com>
2026-01-11 13:23:05 +0100

    Added feature to learn day of weeks

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,62 @@
-
-
+import React from "react";
+import { useState } from "react";
+import { days } from "./dayOfWeeks.js";
 
 function App() {
- 
+  const [index, setIndex] = useState(getRandomIdx(days));
+  const [userInput, setUserInput] = useState("");
+  const handleChange = (e) => {
+    console.log("text:", e.target.value);
+    setUserInput(e.target.value);
+  };
 
+  const onClick = () => {
+    const firstvalue = days[index];
+    const secondvalue = userInput;
+    if (firstvalue === secondvalue) {
+      console.log("correct");
+    } else {
+      console.log("incorrect");
+    }
+  };
+
+  console.log("render");
   return (
     <>
-      <div className="border border-red-400 bg-amber-600">
-       Hello world new
+      <div className="flex justify-center min-h-screen border border-red-400">
+        <div className="flex flex-col justify-between border border-red-400">
+          <div className=" border-2 border-yellow-600">Days of Week</div>
+          <div className="flex flex-col border border-red-400">
+            <div className="border border-red-400 text-center text-xl">
+              {days[index]}
+              {/* Это первое значение */}
+            </div>
+            <input
+              className="w-100 mt-4 text-red-400 border-2 rounded-lg border-emerald-800"
+              type="text"
+              value={userInput}
+              // это второе значение
+              onChange={handleChange}
+              placeholder="введите текст"
+            />
+            <div className="flex justify-center">
+              <button
+                className="mt-4 border-2 border-blue-500 shadow-blue-400 rounded-lg py-2 px-4"
+                onClick={onClick}
+              >
+                check
+              </button>
+            </div>
+          </div>
+          <div className="border border-blue-500">3</div>
+        </div>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+const getRandomIdx = (array) => {
+  return Math.floor(Math.random() * array.length);
+};
+
+export default App;
---
+++ src/dayOfWeeks.js
@@ -0,0 +1,3 @@
+export const days =["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+
+console.log(days[0]);
