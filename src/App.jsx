5bcda63ef0a017215d41af8a9918baef3530a9d5
Dana <dana@example.com>
2026-01-13 23:07:19 +0100

    Added correct/incorrect message

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,12 @@ import { days, daysEs } from "./dayOfWeeks.js";
 function App() {
   const [index, setIndex] = useState(getRandomIdx(days));
   const [userInput, setUserInput] = useState("");
+  const [answer, setAnswer] = useState(null);
+
   const handleChange = (e) => {
     console.log("text:", e.target.value);
     setUserInput(e.target.value);
   };
-
 
   const onClick = () => {
     const firstvalue = daysEs[index];
@@ -17,31 +18,41 @@ function App() {
     const thirdvalue = secondvalue.toLowerCase();
 
     if (firstvalue === thirdvalue) {
-      console.log("correct");
+      setAnswer(true);
     } else {
-      console.log("incorrect");
+      setAnswer(false);
     }
   };
 
-  console.log("render");
   return (
     <>
       <div className="flex justify-center min-h-screen  bg-slate-50">
         <div className="flex flex-col justify-between">
-          <div className="text-3xl text-center text-bold">Days of Week</div>
+          <div className="text-3xl text-center text-bold mt-20">
+            Days of Week
+          </div>
           <div className="flex flex-col ">
-            <div className="text-center text-xl">
+            <div className="text-center text-xl text-indigo-600">
               {days[index]}
               {/* Это первое значение */}
             </div>
             <input
-              className="w-100 mt-4 p-3 text-center border border-gray-300 text-gray-900 rounded-lg  focus:outline-none focus:ring-2 focus:ring-indigo-500 transition"
+              className="w-100 mt-4 p-3 text-xl text-center border border-gray-300 text-gray-900 rounded-lg  focus:outline-none focus:ring-2 focus:ring-indigo-500 transition"
               type="text"
               value={userInput}
               // это второе значение
               onChange={handleChange}
               placeholder="enter day of the week in spanish"
             />
+            <div className="text-center">
+              {answer === null ? (
+                <div>waiting your answer..</div>
+              ) : answer === true ? (
+                <div className="text-green-500">Correct</div>
+              ) : (
+                <div className="text-red-400">"The expected answer is: {daysEs[index]}"</div>
+              )}
+            </div>
             <div className="flex justify-center">
               <button
                 className="mt-4 bg-sky-500 hover:bg-sky-700  rounded-lg py-2 px-4"
@@ -51,7 +62,7 @@ function App() {
               </button>
             </div>
           </div>
-          <div className="border border-blue-500"></div>
+          <div></div>
         </div>
       </div>
     </>
