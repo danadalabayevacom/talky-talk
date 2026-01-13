1a3e8bcc34beec90f7145b7e4c8caba6258ba470
Dana <dana@example.com>
2026-01-14 00:36:54 +0100

    Added counter and next button

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,47 @@ import { days, daysEs } from "./dayOfWeeks.js";
 function App() {
   const [index, setIndex] = useState(getRandomIdx(days));
   const [userInput, setUserInput] = useState("");
-  const [answer, setAnswer] = useState(null);
+  const [answer, setAnswer] = useState(null); // todo rename
+  const [count, setCount] = useState(0);
 
   const handleChange = (e) => {
     console.log("text:", e.target.value);
     setUserInput(e.target.value);
   };
 
-  const onClick = () => {
+  const onClickCheck = () => {
     const firstvalue = daysEs[index];
     const secondvalue = userInput.trim();
     const thirdvalue = secondvalue.toLowerCase();
 
     if (firstvalue === thirdvalue) {
       setAnswer(true);
+      setCount(count + 1); // todo use current state
     } else {
       setAnswer(false);
     }
+  };
+
+  const onClcikNext = () => {
+    setIndex(getRandomIdx(days));
+    setUserInput("");
+    setAnswer(null);
+  };
+
+  const getMessage = (answer) => {
+    let message = null;
+    if (answer === null) {
+      message = <div>waiting your answer..</div>;
+    } else if (answer === true) {
+      message = <div className="text-green-500">Correct</div>;
+    } else {
+      message = (
+        <div className="text-red-400">
+          "The expected answer is: {daysEs[index]}"
+        </div>
+      );
+    }
+    return message;
   };
 
   return (
@@ -44,25 +68,28 @@ function App() {
               onChange={handleChange}
               placeholder="enter day of the week in spanish"
             />
-            <div className="text-center">
-              {answer === null ? (
-                <div>waiting your answer..</div>
-              ) : answer === true ? (
-                <div className="text-green-500">Correct</div>
-              ) : (
-                <div className="text-red-400">"The expected answer is: {daysEs[index]}"</div>
-              )}
-            </div>
-            <div className="flex justify-center">
+            <div className="text-center">{getMessage(answer)}</div>
+            <div className="flex justify-center mt-4">
               <button
-                className="mt-4 bg-sky-500 hover:bg-sky-700  rounded-lg py-2 px-4"
-                onClick={onClick}
+                className="flex-1 bg-sky-500 hover:bg-sky-700  rounded-lg py-2 px-4"
+                onClick={onClickCheck}
+                // todo add clcik action add border
               >
                 check
+              </button>
+              <button
+                className="flex-1 bg-pink-300 hover:bg-pink-400 ml-2 rounded-lg py-2 px-4"
+                onClick={onClcikNext}
+
+              >
+                next
               </button>
             </div>
           </div>
           <div></div>
+          <div className="absolute top-20 right-40 text-violet-400 text-3xl border border-red-400">
+            {count}
+          </div>
         </div>
       </div>
     </>
